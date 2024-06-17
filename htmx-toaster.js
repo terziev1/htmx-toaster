f68db6d9000dd5d66252a313175c0af8f5673346
terziev1 <kostadin_terziev@abv.bg>
2024-06-17 16:15:06 +0300

    add swipe gestures

--- htmx-toaster.js
+++ htmx-toaster.js
@@ -3,6 +3,11 @@ class ToasterComponent extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.toasts = [];
+    this.touchStartX = 0;
+    this.touchEndX = 0;
+    this.currentToast = null;
+    this.isDragging = false;
+    this.dragThreshold = 10;
     this.initializeEventListener();
     this.initializeContainer();
   }
@@ -13,9 +18,16 @@ class ToasterComponent extends HTMLElement {
     this.shadowRoot.appendChild(container);
   }
 
+  generateTheme() {
+    const colors = document.querySelector("script[data-htmxt-colors]")?.getAttribute("data-htmxt-colors") || null;
+    if (!colors) return;
+    const colorArray = colors.split(";");
+    const defaults = ["#f2f2f2", "#000", "#00a96f", "#00b3f0", "#ff6f70"];
+    const [bg, txt, sc, inf, err] = colorArray.map((color, index) => color || defaults[index]);
+    return `.toaster {--bg:${bg};--txt:${txt};--sc:${sc};--in:${inf};--er:${err};}`;
+  }
   initializeEventListener() {
     document.body.addEventListener("htmx:afterRequest", (event) => {
-      console.log("event");
       const body = event.detail.xhr.getResponseHeader("X-Toast-Body");
       const type = event.detail.xhr.getResponseHeader("X-Toast-Type") || "default";
       if (body) {
@@ -26,24 +38,28 @@ class ToasterComponent extends HTMLElement {
 
   addToast({ body, type }) {
     const id = Math.random().toString(36).substr(2, 9);
+    if (this.toasts.length == 3) {
+      console.log("removing toast", this.toasts[this.toasts.length - 1].id, this.toasts.length);
+      this.removeToast(this.toasts[this.toasts.length - 1].id);
+    }
     this.toasts.unshift({ id, body, type });
     this.renderToast(id);
-    if (this.toasts.length > 3) {
-      this.removeToast(this.toasts.pop().id);
-    }
   }
 
   removeToast(id) {
     const index = this.toasts.findIndex((toast) => toast.id === id);
-    if (index !== -1) {
-      const toastElement = this.shadowRoot.querySelector(`#toast-${id}`);
+    if (index == -1) return;
+    this.toasts.splice(index, 1);
+    const toastElement = this.shadowRoot.querySelector(`#toast-${id}`);
+    if (toastElement) {
       toastElement.classList.add("fade-out");
       toastElement.addEventListener("animationend", () => {
-        toastElement.remove();
-        this.toasts.splice(index, 1);
+        this.shadowRoot.querySelector(".toaster").removeChild(toastElement);
+        console.log("Toast removed from DOM:", id);
       });
     }
   }
+
   closeButton(id) {
     return `<button class="close-btn" style="padding:4px;transition:opacity 0.3s;position:absolute;top:2px;right:2px;" type="button" class="close" onclick="document.querySelector('htmx-toaster-component').removeToast('${id}')"> <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4"><path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></button>`;
   }
@@ -57,12 +73,16 @@ class ToasterComponent extends HTMLElement {
         ${this.closeButton(id)}
       `;
     this.shadowRoot.querySelector(".toaster").appendChild(toastElement);
-    setTimeout(() => this.removeToast(id), 5000); // Auto-remove after 5 seconds
+    toastElement.addEventListener("touchstart", (event) => this.handleTouchStart(event, toastElement), { passive: true });
+    toastElement.addEventListener("touchmove", (event) => this.handleTouchMove(event), { passive: true });
+    toastElement.addEventListener("touchend", (event) => this.handleTouchEnd(event, id), { passive: true });
+    setTimeout(() => this.removeToast(id), 5000000); // Auto-remove after 5 seconds
   }
 
   connectedCallback() {
     const style = document.createElement("style");
     style.textContent = `
+     ${this.generateTheme()}
         .toaster * { margin: 0; text-decoration: none; box-sizing: border-box; font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;}
         .toaster {
             display: flex;
@@ -91,17 +111,17 @@ class ToasterComponent extends HTMLElement {
             font-size: 1rem;
             line-height: 1.5rem;
             z-index: 50;
-            color: hsl(0, 0%, 12%);
+            color: var(--txt,#000);
             text-align: left;
             animation: slideIn 0.3s ease-in-out;
             user-select: auto;
             pointer-events: auto;
             transition: transform 0.3s ease;
         }
-        .default { background-color: white; }
-        .success { background-color: lightgreen; }
-        .info { background-color: lightblue; }
-        .error { background-color: salmon; }
+        .default { background-color:var(--bg,#f2f2f2) }
+        .success { background-color:var(--sc, #00a96f); }
+        .info { background-color:var(--in, #00b3f0); }
+        .error { background-color: var(--er,#ff6f70); }
         @media (min-width: 640px) {
             .toaster { inset: 3rem; }
             .toast { max-width: 350px; }
@@ -124,6 +144,45 @@ class ToasterComponent extends HTMLElement {
         @keyframes fadeOut { from { opacity: 1; } to { opacity: 0; } }`;
 
     this.shadowRoot.appendChild(style);
+  }
+
+  handleTouchStart(event, toastElement) {
+    this.touchStartX = event.changedTouches[0].screenX;
+    this.currentToast = toastElement;
+    this.isDragging = false; // Reset dragging flag on touch start
+  }
+
+  handleTouchMove(event) {
+    if (!this.currentToast) return;
+    this.touchEndX = event.changedTouches[0].screenX;
+    const touchMoveX = this.touchEndX - this.touchStartX;
+    if (Math.abs(touchMoveX) > this.dragThreshold) {
+      this.isDragging = true; // Set dragging flag if movement exceeds threshold
+      this.currentToast.style.transform = `translateX(${touchMoveX}px)`;
+    }
+  }
+
+  handleTouchEnd(event, id) {
+    if (!this.currentToast) return;
+    const touchMoveX = this.touchEndX - this.touchStartX;
+    const swipeThreshold = 100; // Swipe threshold in pixels
+
+    if (this.isDragging && Math.abs(touchMoveX) > swipeThreshold) {
+      this.removeToast(id);
+    } else {
+      this.currentToast.style.transform = "";
+      this.currentToast.style.transition = "transform 0.3s ease";
+      setTimeout(() => {
+        if (this.currentToast) {
+          this.currentToast.style.transition = "";
+        }
+      }, 300);
+    }
+
+    this.currentToast = null;
+    this.touchStartX = 0;
+    this.touchEndX = 0;
+    this.isDragging = false; // Reset dragging flag on touch end
   }
 }
 

e6c40f0bb01b30fc7e3b4db4eac27e57d6fc8196
terziev1 <kostadin_terziev@abv.bg>
2025-02-17 11:02:49 +0200

    deploy fix for hx boost

--- htmx-toaster.js
+++ htmx-toaster.js
@@ -65,7 +65,7 @@ class ToasterComponent extends HTMLElement {
   }
 
   closeButton(id) {
-    return `<button class="close-btn" style="padding:4px;position:absolute;top:2px;right:2px;" type="button"  onclick="document.querySelector('htmx-toaster-component').removeToast('${id}')"> <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4"><path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></button>`;
+    return `<button class="close-btn" style="padding:4px;position:absolute;top:2px;right:2px;" type="button"  onclick="document.querySelector('htmx-toaster-component').removeToast('${id}')">&#10005;</button>`;
   }
   renderToast(id) {
     const { body, type } = this.toasts.find((toast) => toast.id === id);
@@ -174,9 +174,23 @@ class ToasterComponent extends HTMLElement {
   }
 }
 
-customElements.define("htmx-toaster-component", ToasterComponent);
-window.addEventListener("DOMContentLoaded", () => {
+if (customElements && !customElements.get("shopbox-results")) {
+  customElements.define("htmx-toaster-component", ToasterComponent);
+}
+
+function createToasterComponent() {
   const toasterElement = document.createElement("htmx-toaster-component");
+  toasterElement.id = "htmx-toaster";
   window.HTMXToaster = toasterElement;
   document.body.appendChild(toasterElement);
+}
+window.addEventListener("DOMContentLoaded", () => {
+  createToasterComponent();
+});
+
+htmx.on("htmx:afterSettle", () => {
+  if (document.getElementById("htmx-toaster")) {
+    return;
+  }
+  createToasterComponent();
 });
